8575c86470fecb875a6f67253224b87881f52be8
gustavo <gustavopetruzzi19@gmail.com>
2018-09-05 12:03:35 -0300

    parece que anda el login, splash e icono

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,14 @@ import { SplashPage } from '../pages/splash/splash';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = SplashPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
+      splashScreen.hide();
     });
   }
 }
